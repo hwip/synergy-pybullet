57cc5625d0e742844dcd8c72065a33db7b6ddd86
Tomohiro MOTODA <motoda.populus14144@gmail.com>
2020-06-08 16:30:57 +0900

    Modify the detail

--- synergyenvs/synergyenvs/envs/tasks/grasp_box_env.py
+++ synergyenvs/synergyenvs/envs/tasks/grasp_box_env.py
@@ -11,7 +11,7 @@ class GraspBoxEnv(BaseBulletEnv):
         self.camera = Camera(self)
 
     def create_single_player_scene(self, bullet_client):
-        return SingleRobotEmptyScene(bullet_client, gravity=0.0, timestep=0.0020, frame_skip=5)
+        return SingleRobotEmptyScene(bullet_client, gravity=9.81, timestep=0.0020, frame_skip=5)
 
     def step(self, a):
         self.robot.apply_action(a)
